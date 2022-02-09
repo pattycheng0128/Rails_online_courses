b65670c154a209b62cf5fae2ba2c9ebbfcdc74d4
patty <pattycheng2008@gmail.com>
2022-02-09 17:37:15 +0800

    add gem grape

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
+  # courses
   root "courses#index"
   resources :courses
 
-  #users
+  # users
   get "/sign_up", to: "users#sign_up"
   post "/account_verify", to: "users#account_verify"
 
