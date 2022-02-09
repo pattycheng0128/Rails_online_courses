a9d75d5952dcb3eef5f9c09e430cee1f8cb955f8
patty <pattycheng2008@gmail.com>
2022-02-09 15:13:51 +0800

    add_users

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,13 @@ Rails.application.routes.draw do
   root "courses#index"
   resources :courses
 
+  #users
+  get "/sign_up", to: "users#sign_up"
+  post "/account_verify", to: "users#account_verify"
+
+  get "/sign_in", to: "users#sign_in"
+  post "sign_in/check", to: "users#check"
+  
+  delete "/sign_out", to: "users#sign_out"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_022305) do
+ActiveRecord::Schema.define(version: 2022_02_09_055522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2022_02_09_022305) do
     t.string "courses_type"
     t.string "url"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_courses_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
