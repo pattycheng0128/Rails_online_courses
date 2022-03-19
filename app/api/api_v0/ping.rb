b5a0ea77a3d8b2ee17a3bebffaafa7df57a34294
patty <pattycheng2008@gmail.com>
2022-03-19 09:38:59 +0800

    add commit

--- app/api/api_v0/ping.rb
+++ app/api/api_v0/ping.rb
@@ -1,6 +1,7 @@
 module ApiV0
   class Ping < Grape::API
     desc 'Ping pong'
+    # ip/api/v0/ping
     get "/ping" do
       "test grape api"
       # authenticate!
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,6 @@
 const { environment } = require('@rails/webpacker')
 
-var webpack = require('webpack');
+const webpack = require('webpack');
 environment.plugins.append( 'Provide',
     new webpack.ProvidePlugin({
         $: 'jquery',
